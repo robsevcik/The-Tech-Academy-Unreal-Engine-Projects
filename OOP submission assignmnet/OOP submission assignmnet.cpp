45c225e89d5c34f0cb1ac14fa87133d66da7bf76
robsevcik <robert.a.sevcik@gmail.com>
2021-10-07 07:28:08 -0700

    draft3

--- OOP submission assignmnet/OOP submission assignmnet.cpp
+++ OOP submission assignmnet/OOP submission assignmnet.cpp
@@ -7,21 +7,24 @@ public:
 	string Color;
 	string Height;
 	string weight;
+
+
+
+	void myFunction() {
+		cout << "The dog is sitting \n";
+		cout << "The dog is shaking \n";
+		cout << "The dog is laying down";
+
+	}
 };
 
-void myFunction() {
-	cout << "The dog is sitting \n";
-	cout << "The dog is shaking \n";
-	cout << "The dog is laying down";
-
-}
 int main() {
 	dog dogObj;
 	dogObj.Breed = "Hound";
 	dogObj.Color = "brown";
 	dogObj.Height = "2 feet";
 	dogObj.weight = "60 lbs";
-	myFunction();
+	
 }
 
 
